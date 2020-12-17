82fe3f416b504a45758f05540b64ecd3656b45da
dfgdfgertt <majinsyn@gmail.com>
2020-12-18 00:27:59 +0700

    hihi

--- product/config.js
+++ product/config.js
@@ -1,11 +1,19 @@
 var ip = require("ip");
     console.dir ( ip.address() );
 
+var publicIp = require( 'public-ip' );
+
+(async () => {
+  console.log( await publicIp.v4());
+  var ippublic = await publicIp.v4();
+  console.log(ippublic);
+})();
+
 module.exports = {
     aws_table_name: 'product',
     aws_local_config: {
         region: 'local',
-        endpoint: 'http://${ip.address()}:3000'
+        endpoint: 'http://${ippublic}:3000'
       //Provide details for local configuration
     },
     aws_remote_config: {
