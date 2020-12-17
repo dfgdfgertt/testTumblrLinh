83a2aa9712de6539792110af0d27616bdb935fec
Majin Syn <43411091+dfgdfgertt@users.noreply.github.com>
2020-12-17 23:59:10 +0700

    Update config.js

--- product/config.js
+++ product/config.js
@@ -2,7 +2,7 @@ module.exports = {
     aws_table_name: 'product',
     aws_local_config: {
         region: 'local',
-        endpoint: 'http://54.169.36.51:3000'
+        endpoint: 'http://APP_PRIVATE_IP_ADDRESS:3000'
       //Provide details for local configuration
     },
     aws_remote_config: {
